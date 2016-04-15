b35e665147f8075cc773a452641ccfb7d567bb81
Terrell Broomer <sftmb4@gmail.com>
2016-04-15 03:58:39 -0400

    adding warden pieces for now

--- Gemfile
+++ Gemfile
@@ -4,3 +4,11 @@ gem 'sinatra-r18n'
 gem 'unicorn'
 gem 'unicorn-worker-killer'
 gem 'haml'
+
+group 'auth' do
+gem 'warden'
+gem 'sinatra-flash'
+gem 'bcrypt'
+gem 'data_mapper'
+gem 'dm-sqlite-adapter'
+end
--- myapp.rb
+++ myapp.rb
@@ -1,11 +1,89 @@
 require 'sinatra/base'
 require 'sinatra/r18n'
 require 'haml'
+require 'warden'
+require 'sinatra/flash'
+require_relative 'model'
+
+Warden::Strategies.add(:password) do
+  def valid?
+    params['user'] && params['user']['username'] && params['user']['password']
+  end
+
+  def authenticate!
+    user = User.first(username: params['user']['username'])
+
+    if user.nil?
+      throw(:warden, message: "The username you entered does not exist.")
+    elsif user.authenticate(params['user']['password'])
+      success!(user)
+    else
+      throw(:warden, message: "The username and password combination ")
+    end
+  end
+end
 
 class MyApp < Sinatra::Base
   register Sinatra::R18n
   use Rack::Session::Cookie, :key => 'rack.session'
+  register Sinatra::Flash
 
+  use Warden::Manager do |config|
+    # Tell Warden how to save our User info into a session.
+    # Sessions can only take strings, not Ruby code, we'll store
+    # the User's `id`
+    config.serialize_into_session{|user| user.id }
+    # Now tell Warden how to take what we've stored in the session
+    # and get a User from that information.
+    config.serialize_from_session{|id| User.get(id) }
+
+    config.scope_defaults :default,
+      # "strategies" is an array of named methods with which to
+      # attempt authentication. We have to define this later.
+      strategies: [:password],
+      # The action is a route to send the user to when
+      # warden.authenticate! returns a false answer. We'll show
+      # this route below.
+      action: 'auth/unauthenticated'
+    # When a user tries to log in and cannot, this specifies the
+    # app to send the user to.
+    config.failure_app = self
+  end
+
+  Warden::Manager.before_failure do |env,opts|
+    env['REQUEST_METHOD'] = 'POST'
+  end
+
+  # Warden routes
+  post '/auth/login' do
+    env['warden'].authenticate!
+    flash[:success] = "Successfully logged in"
+    if session[:return_to].nil?
+      redirect '/'
+    else
+      redirect session[:return_to]
+    end
+  end
+
+  get '/auth/logout' do
+    env['warden'].raw_session.inspect
+    env['warden'].logout
+    flash[:success] = 'Successfully logged out'
+    redirect '/'
+  end
+
+
+  post '/auth/unauthenticated' do
+    session[:return_to] = env['warden.options'][:attempted_path] if session[:return_to].nil?
+
+    # Set the error and use a fallback if the message is not defined
+    flash[:error] = env['warden.options'][:message] || "You must log in"
+    redirect '/auth/login'
+  end
+
+
+  # App routes
+=begin
   get '/' do
     haml :index
   end
@@ -13,5 +91,20 @@ class MyApp < Sinatra::Base
   get '/lang/:locale' do
     session[:locale] = params[:locale]
     redirect back
+  end
+=end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/auth/login' do
+    erb :login
+  end
+
+  get '/protected' do
+    env['warden'].authenticate!
+    @current_user = env['warden'].user
+    erb :protected
   end
 end
